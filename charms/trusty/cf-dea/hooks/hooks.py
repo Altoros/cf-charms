bb941265307927e82c5d1c559a567288a6b0c580
Alexander Prismakov <prismakov@gmail.com>
2014-03-15 15:29:42 +0300

    DEA charm. NATS-DEA relation

--- charms/trusty/cf-dea/hooks/hooks.py
+++ charms/trusty/cf-dea/hooks/hooks.py
@@ -11,12 +11,12 @@ import shutil
 import cPickle as pickle
 
 from charmhelpers.core import hookenv, host
-from charmhelpers.core.hookenv import log, DEBUG, ERROR
+from charmhelpers.core.hookenv import log, DEBUG, ERROR, WARNING
 
 from charmhelpers.fetch import (
     apt_install, apt_update, add_source, filter_installed_packages
 )
-# from utils import render_template
+from utils import render_template
 
 
 hooks = hookenv.Hooks()
@@ -83,10 +83,36 @@ def run(command, exit_on_error=True, quiet=False):
 
 
 def emit_deaconf():
-    #deacontext = {}
-    #with open(DEA_CONFIG_FILE, 'w') as natsconf:
-    #    natsconf.write(render_template('nats.yml', natscontext))
-    pass
+    deacontext = {}
+    success = True
+    if 'nats_user' in local_state:
+        deacontext.setdefault('nats_user', local_state['nats_user'])
+    else:
+        success = False
+    if 'nats_password' in local_state:
+        deacontext.setdefault('nats_password', local_state['nats_password'])
+    else:
+        success = False
+    if 'nats_port' in local_state:
+        deacontext.setdefault('nats_port', local_state['nats_port'])
+    else:
+        success = False
+    if 'nats_address' in local_state:
+        deacontext.setdefault('nats_address', local_state['nats_address'])
+    else:
+        success = False
+    if success:
+        log('Emit dea conf successfull')
+        with open(DEA_CONFIG_PATH, 'w') as deaconf:
+            deaconf.write(render_template('dea.yml', deacontext))
+        local_state['config_ok'] = 'true'
+        return True
+    else:
+        if 'config_ok' in local_state:
+            del local_state['config_ok']
+            local_state.save()
+        log('Emit dea conf unsuccessfull', WARNING)
+        return False
 
 
 def chownr(path, owner, group):
@@ -104,63 +130,83 @@ def install():
     add_source(config_data['source'], config_data['key'])
     apt_update(fatal=True)
     apt_install(packages=filter_installed_packages(DEA_PACKAGES), fatal=True)
+    install_upstart_scripts()
     host.adduser('vcap')
-    dirs = [DEA_DIR]
+    host.mkdir(CF_DIR, owner='vcap', group='vcap', perms=0775)
+    os.chdir(CF_DIR)
+    run(['gem', 'install', 'bundle', 'eventmachine'])
+    run(['gem', 'install', 'bundle'])
+    run(['git', 'clone', 'https://github.com/cloudfoundry/dea_ng.git'])
+    dirs = [DEA_PIDS_DIR, DEA_CACHE_DIR, DEA_BP_CACHE_DIR]
     for item in dirs:
         host.mkdir(item, owner='vcap', group='vcap', perms=0775)
-    #install_upstart_scripts()
+    os.chdir(DEA_DIR)
+    run(['git', 'submodule', 'update', '--init'])
+    run(['bundle', 'install', '--without', 'test'])
+    chownr(CF_DIR, 'vcap', 'vcap')
 
 
 @hooks.hook()
 def start():
-    log("Starting DEA as upstart job")
-    #host.service_start('cf-dea')
+    if 'config_ok' in local_state:
+        if not host.service_running('cf-dea'):
+            #hookenv.open_port(local_state['router_port'])
+            log("Starting DEA as upstart job")
+            host.service_start('cf-dea')
 
 
 @hooks.hook("config-changed")
 def config_changed():
-    pass
+    #port_config_changed('router_port')
+    emit_deaconf()
+    if host.service_running('cf-dea'):
+        #TODO replace with config reload
+        host.service_restart('cf-dea')
 
 
 @hooks.hook()
 def stop():
-    host.service_stop('cf-dea')
-    #hookenv.close_port(local_state['nats_port'])
+    if host.service_running('cf-dea'):
+        host.service_stop('cf-dea')
+#        hookenv.close_port(local_state['router_port'])
 
 
 @hooks.hook('nats-relation-changed')
 def nats_relation_changed():
     for relid in hookenv.relation_ids('nats'):
-        log('NATS address:' + local_state['nats_address'] + ':'
-            + str(local_state['nats_port']), DEBUG)
-        log('NATS user:' + local_state['nats_user'] + ':'
-            + str(local_state['nats_password']), DEBUG)
-        hookenv.relation_set(relid,
-                             nats_address=local_state['nats_address'],
-                             nats_port=local_state['nats_port'],
-                             nats_user=local_state['nats_user'],
-                             nats_password=local_state['nats_password'],
-                             )
+        #TODO add checks of values
+        local_state['nats_address'] = hookenv.relation_get('nats_address')
+        local_state['nats_port'] = hookenv.relation_get('nats_port')
+        local_state['nats_user'] = hookenv.relation_get('nats_user')
+        local_state['nats_password'] = hookenv.relation_get('nats_password')
+        local_state.save()
+    if emit_deaconf():
+        start()
 
 
-@hooks.hook('nats-relation-joined')
-def nats_relation_joined():
-    log('Hi from joined hook')
+@hooks.hook('nats-relation-departed')
+def nats_relation_departed():
+    log('Hi from departed hook')
 
 
 @hooks.hook('nats-relation-broken')
 def nats_relation_broken():
-    log('Hi from broken hook')
+    stop()
 
 
 #################### Global variables ####################
 config_data = hookenv.config()
 hook_name = os.path.basename(sys.argv[0])
 #TODO replace with actual dea package
-DEA_PACKAGES = ['git', 'ruby1.9.1-dev', 'libxslt-dev', 'libxml2-dev']
+DEA_PACKAGES = ['g++', 'make', 'git', 'ruby1.9.1-dev', 'libxslt-dev',
+                'libxml2-dev']
 
 CF_DIR = '/var/lib/cloudfoundry'
-DEA_DIR = os.path.join(CF_DIR, 'dea-ng')
+DEA_DIR = os.path.join(CF_DIR, 'dea_ng')
+DEA_PIDS_DIR = os.path.join(DEA_DIR, 'pids')
+DEA_CACHE_DIR = os.path.join(DEA_DIR, 'cache')
+DEA_BP_CACHE_DIR = os.path.join(DEA_DIR, 'buildpack_cache')
+DEA_CONFIG_PATH = os.path.join(DEA_DIR, 'config', 'dea.yml')
 local_state = State('local_state.pickle')
 
 if __name__ == '__main__':
