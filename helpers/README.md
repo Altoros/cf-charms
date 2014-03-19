30c59fe248402460152e9fe6195f8fd517498b27
A.S. Lomoff <lomov.as@gmail.com>
2014-03-19 05:19:54 +0300

    last changes for today

---
+++ helpers/README.md
@@ -0,0 +1,15 @@
+CF Charms helpers
+===
+
+Description
+---
+This folder contains python helpers for cf-charms project and utils that help deploy them.
+
+
+Utilities
+---
+`update_helpers.py` - copies all helpers to charms (idea is take from charmshelpers). It can be called from any folder.
+`update_hosts.py` - updates `/etc/hosts` for every service machine. It makes `example.net` to resolve with router ip. It should be run from machine bootstraped with juju. Sccript fetches all necessary config from `juju status` command.
+
+Usage
+---
--- helpers/ports_helper.py
+++ helpers/ports_helper.py
@@ -1,11 +1,17 @@
 
-def port_config_changed(port):
+
+def port_config_changed(port, hookenv):
     '''Cheks if value of port changed close old port and open a new one'''
     if port in local_state:
         if local_state[port] != config_data[port]:
-            hookenv.close_port(local_state[port])
-            local_state[port] = config_data[port]
-    else:
-        local_state[port] = config_data[port]
-    local_state.save()
+            log('Stored value for {} isn\'t equal to config data'.format(port),
+                DEBUG)
+            log('Closing port {}'.format(str(local_state[port])), WARNING)
+            try: 
+                hookenv.close_port(local_state[port])
+            except:
+                log('{} port is not closed.'.format(str(local_state[port])), WARNING)
+
     hookenv.open_port(config_data[port])
+    local_state[port] = config_data[port]
+    local_state.save()
