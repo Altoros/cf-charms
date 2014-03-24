7772708f7592fb3be2a8cb5f7cb3113545cbe4eb
Alex Prismakov <prismakov@gmail.com>
2014-03-24 19:29:51 +0000

    Makefiles updated

--- charms/trusty/cf-go-router/hooks/hooks.py
+++ charms/trusty/cf-go-router/hooks/hooks.py
@@ -3,10 +3,7 @@
 
 import os
 import sys
-import time
-import glob
 import subprocess
-import shutil
 from cloudfoundry import ROUTER_PACKAGES
 from charmhelpers.core import hookenv, host
 from charmhelpers.payload.execd import execd_preinstall
@@ -17,15 +14,11 @@ from helpers.upstart_helper import install_upstart_scripts
 
 from charmhelpers.core.hookenv import \
     (
-        CRITICAL, ERROR, WARNING, INFO, DEBUG, log,
+        ERROR, WARNING, DEBUG, log,
     )
 from charmhelpers.fetch import (
     apt_install,
-    apt_update,
-    filter_installed_packages,
-    add_source
 )
-from utils import render_template
 from cloudfoundry import chownr
 
 
@@ -61,7 +54,6 @@ def Template(*args, **kw):
     """
     from jinja2 import Template
     return Template(*args, **kw)
-
 
 
 def run(command, exit_on_error=True, quiet=False):
@@ -100,12 +92,12 @@ def run(command, exit_on_error=True, quiet=False):
 
 def emit_router_config():
 
-    config_items = ['nats_user', 'nats_password', 'nats_port', 'nats_address', 'router_port',
-                    'router_status_port', 'router_status_user', 'router_status_password']
+    config_items = ['nats_user', 'nats_password', 'nats_port', 'nats_address',
+                    'router_port', 'router_status_port', 'router_status_user',
+                    'router_status_password']
 
     emit_config('router', config_items, local_state,
                 'gorouter.yml', ROUTER_CONFIG_FILE)
-
 
 
 hooks = hookenv.Hooks()
@@ -127,10 +119,12 @@ def install():
     os.environ['GOPATH'] = CF_DIR
     os.environ["PATH"] = CF_DIR + os.pathsep + os.environ["PATH"]
     #ToDo: git clone is nor idempotent. If repo dir exists it exits with error
-    #fix it by deleting 'src' directory before. This won't allow to redeploy to an existing machine
+    #fix it by deleting 'src' directory before. This won't allow
+    # to redeploy to an existing machine
     os.chdir(CF_DIR + '/src/github.com/cloudfoundry')
     # TODO: check if repo already exists
-    # CAUSES ERROR fatal: destination path 'gorouter' already exists and is not an empty directory.
+    # CAUSES ERROR fatal: destination path 'gorouter' already exists
+    # and is not an empty directory.
     run(['git', 'clone', 'https://github.com/cloudfoundry/gorouter.git'])
     os.chdir(CF_DIR + '/src/github.com/stretchr/')
     run(['git', 'clone', 'https://github.com/stretchr/objx.git'])
@@ -150,10 +144,11 @@ def port_config_changed(port):
             log('Stored value for {} isn\'t equal to config data'.format(port),
                 DEBUG)
             log('Closing port {}'.format(str(local_state[port])), WARNING)
-            try: 
+            try:
                 hookenv.close_port(local_state[port])
             except:
-                log('{} port is not closed.'.format(str(local_state[port])), WARNING)
+                log('{} port is not closed.'.format(str(local_state[port])),
+                    WARNING)
 
     hookenv.open_port(config_data[port])
     local_state[port] = config_data[port]
@@ -172,21 +167,22 @@ def start():
 def config_changed():
     local_state['router_ok'] = False
 
-    config_items = ['nats_user', 'nats_password', 'nats_port', 'nats_address', 'router_port',
-                    'router_status_port', 'router_status_user', 'router_status_password']
-    
+    config_items = ['nats_user', 'nats_password', 'nats_port', 'nats_address',
+                    'router_port', 'router_status_port', 'router_status_user',
+                    'router_status_password']
+
     for key in config_items:
         value = find_config_parameter(key, hookenv, config_data)
         log(("%s = %s" % (key, value)), DEBUG)
         local_state[key] = value
 
-
     local_state.save()
     port_config_changed('router_port')
-    emit_router_config()    
+    emit_router_config()
 
     stop()
     start()
+
 
 @hooks.hook()
 def stop():
@@ -218,7 +214,7 @@ def router_relation_joined():
 ############################### Global variables ############
 ROUTER_PATH = '/var/lib/cloudfoundry/cfgorouter'
 CF_DIR = '/var/lib/cloudfoundry'
-ROUTER_CONFIG_FILE='/var/lib/cloudfoundry/config/gorouter.yml'
+ROUTER_CONFIG_FILE = '/var/lib/cloudfoundry/config/gorouter.yml'
 local_state = State('local_state.pickle')
 hook_name = os.path.basename(sys.argv[0])
 config_data = hookenv.config()
